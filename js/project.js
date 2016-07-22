77263db83f15dc394bfc296d1e7392ac50889d6d
Hart Liddell <hart@leapingfish.io>
2016-07-22 15:35:42 -0400

    harts updates

--- js/project.js
+++ js/project.js
@@ -1,12 +1,8 @@
-var footLength;
-var footWidth;
-var calculateSquareHorFootage;
-
 function calculateSquareHorFootage() {
-	document.body.backgroundColor = "blue";
-	footLength = document.getElementById("footLength").value;
-	footWidth = document.getElementById("footWidth").value;
+	var footLength = document.getElementById("footLength").value;
+	var footWidth = document.getElementById("footWidth").value;
 	var calculateSquareHorFootage = footWidth * footLength;
 	document.getElementById("sqFootage").innerHTML = calculateSquareHorFootage;
 }
+
 document.getElementById("clickme").onclick = calculateSquareHorFootage;
