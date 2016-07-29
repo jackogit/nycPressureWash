46a4dd2b515f2c7f85cce0999365201a77530e98
Jack Carpenter <lynxster222@gmail.com>
2016-07-29 09:04:01 -0400

    prob with js
    trying dif stuff but not getting far

--- js/project.js
+++ js/project.js
@@ -1,8 +1,18 @@
-function calculateSquareHorFootage() {
-	var footLength = document.getElementById("footLength").value;
-	var footWidth = document.getElementById("footWidth").value;
-	var calculateSquareHorFootage = footWidth * footLength;
-	document.getElementById("sqFootage").innerHTML = calculateSquareHorFootage;
-}
+$(document).ready(function(){
+	$(".time").mouseover(function(){
+		$(this).animate({
+			"color" : "blue"
+		})
+	});
+	
+});
 
-document.getElementById("clickme").onclick = calculateSquareHorFootage;
+ function calculateSquareHorFootage() {
+ 	var footLength = document.getElementById("footLength").value;
+ 	var footWidth = document.getElementById("footWidth").value;
+	var calculateSquareHorFootage = footWidth * footLength;
+ 	document.getElementById("sqFootage").innerHTML = calculateSquareHorFootage;
+ }
+
+ document.getElementById("clickme").onclick = calculateSquareHorFootage;
+
